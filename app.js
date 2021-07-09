6e9cc67725090c5d8e965caad0778bfaf55ce667
benoitD03 <bens.92@hotmail.fr>
2021-07-09 15:08:38 +0200

    writting on inputs ok

--- app.js
+++ app.js
@@ -36,12 +36,12 @@ minusBtn.addEventListener('click', addRemoveColor);
 function addRemoveColor(e) {
 
     alertMsg.innerText = "";
-    const colorInputs = document.querySelectorAll('.color');
-    const randomColor = Math.floor(Math.random()*18379314).toString(16);
+    const inputsColor = document.querySelectorAll('.color');
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
     //AJOUT
     if (e.target.className === "plus") {
 
-        if (colorInputs.length >= 8) {
+        if (inputsColor.length >= 8) {
             alertMsg.innerText = "Vous ne pouvez pas générer plus de 8 couleurs";
             return;
         }
@@ -52,39 +52,63 @@ function addRemoveColor(e) {
         newColor.setAttribute('data-index', indexColor);
         newColor.setAttribute('class', 'color');
         newColor.setAttribute('maxlength', 7);
+        console.log(newColor);
         colorContainer.appendChild(newColor);
 
         allColors.push(newColor.value);
         bodyBackground.style.background = `linear-gradient(${inclinaison}deg, ${allColors})`;
         indexColor++;
- 
+        
+        newColor.addEventListener('input', updateColors);
+        
+
     }
+
     //SUPPRESSION
     else if (e.target.className === "minus") {
 
-        if (colorInputs.length === 2) {
+        if (inputsColor.length === 2) {
             alertMsg.innerText = "Vous devez sélectionner au minimum 2 couleurs.";
         } else {
-            colorInputs[colorInputs.length -1].remove();
             allColors.pop();
-            bodyBackground.style.background = `linear-gradient(${inclinaison}deg, ${allColors})`;
+            inputsColor[inputsColor.length -1].remove();
             indexColor--;
+            bodyBackground.style.background = `linear-gradient(${inclinaison}deg, ${allColors})`;
+            console.log(inputsColor.length);
         }
     }
+    inputsColor.forEach(input => {
+        input.addEventListener('input', updateColors);
+    });
 
 }
 
+colorInputs.forEach(input => {
+    input.addEventListener('input', updateColors);
+});
+
+//******* Rentrer manuellement des couleurs  *******
+function updateColors(e) {
+
+    let currentIndex = e.target.getAttribute('data-index');
+    
+    e.target.value = e.target.value.toUpperCase();
+    allColors[currentIndex - 1] = e.target.value.toUpperCase();
+    e.target.style.background = allColors[currentIndex - 1];
+    bodyBackground.style.background = `linear-gradient(${inclinaison}deg, ${allColors})`;
+
+}
 
 //******* Afficher des couleurs aléatoires *******
 randomBtn.addEventListener('click', () => {
 
-    const colorInputs = document.querySelectorAll('.color');
+    const colorsInputs = document.querySelectorAll('.color');
 
     for(let i = 0; i < allColors.length; i++) {
 
-        allColors[i] = `#${Math.floor(Math.random()*18379314).toString(16)}`;
-        colorInputs[i].value = allColors[i].toUpperCase();
-        colorInputs[i].style.background = allColors[i].toUpperCase();
+        allColors[i] = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+        colorsInputs[i].value = allColors[i].toUpperCase();
+        colorsInputs[i].style.background = allColors[i].toUpperCase();
         bodyBackground.style.background = `linear-gradient(${inclinaison}deg, ${allColors})`;
 
     }
